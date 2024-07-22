d79fa0ba1706e99c7980c240546935e955944a9b
Aditya Kumar AKT <168528524+AdityaKumarAKT@users.noreply.github.com>
2024-07-22 17:25:49 +0530

    Create index.php

---
+++ index.php
@@ -0,0 +1,9 @@
+<html>
+  <header>
+    <title>LND College Motihari - bca24lnd</title>
+  </header>
+  <body><center>
+      <h2>LND College Motihari - bca24lnd</h2>
+    <h1>LND College Motihari - bca24lnd</h1>
+  <center></body>
+</html>
